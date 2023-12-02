76c4e76e0636e89b6ae72c11993de9da5451dd5c
Cbakum <cbakum@gmail.com>
2023-12-01 21:57:02 -0500

    Updated formatting

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,14 +1,12 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 
-
-fn main() -> io::Result<()> { 
+fn main() -> io::Result<()> {
     // initialize lookup table to search for numbers in string
     let mut table: LookupTable = load_table(); // Must be mutable for part two
-    
+
     // ↓↓↓↓ For part two only ↓↓↓↓
-    update_table(&mut table);       // Comment this out for part one
-    // ↑↑↑↑ For part two only ↑↑↑↑
+    update_table(&mut table); // Comment this out for part one
 
     // open file to search through data
     let filename: String = "data.txt".to_string();
@@ -21,7 +19,7 @@ fn main() -> io::Result<()> {
     // Iterate through each line
     for (_line_number, line) in reader.lines().enumerate() {
         let line = line?; // If line is error, halt program
-        
+
         // Process each line here
         running_count += process_line(&line, &table);
     }
@@ -32,18 +30,17 @@ fn main() -> io::Result<()> {
 
 /// Searches a line for the first and last digit within it that is contained
 /// in the lookup table
-/// 
+///
 /// Takes these digits and returns a two digit number
-fn process_line(line: &String, table: &LookupTable) -> u32 {
+fn process_line(line: &str, table: &LookupTable) -> u32 {
     let mut first: Entry = Entry::new();
     let mut last: Entry = Entry::new();
-    
+
     // Change from default to search for highest index for last
     last.index = 0;
 
     // Check all elements in lookup table for each line
     for &(ref text, number) in &table.elements {
-
         if let Some(index) = find_substring_first(line, text) {
             if index < first.index {
                 first.number = number;
@@ -57,29 +54,25 @@ fn process_line(line: &String, table: &LookupTable) -> u32 {
             }
         }
     }
-    if last.index == 0 {
-        last.number = first.number;
-        last.index = first.index;
-    }
-    
+
     // Return two digit number
     (first.number * 10) + last.number
 }
 
-/// Finds the index of the first instance of a string slice within a line. 
-/// 
+/// Finds the index of the first instance of a string slice within a line.
+///
 /// Uses an option to return Some(index) if found and None otherwise
-fn find_substring_first(line: &str, element: &str) ->Option<u32> {
+fn find_substring_first(line: &str, element: &str) -> Option<u32> {
     match line.find(element) {
         Some(index) => Some(index as u32),
         None => None,
     }
 }
 
-/// Finds the index of the last instance of a string slice within a line. 
-/// 
+/// Finds the index of the last instance of a string slice within a line.
+///
 /// Uses an option to return Some(index) if found and None otherwise
-fn find_substring_last(line: &str, element: &str) ->Option<u32> {
+fn find_substring_last(line: &str, element: &str) -> Option<u32> {
     match line.rfind(element) {
         Some(index) => Some(index as u32),
         None => None,
@@ -87,10 +80,10 @@ fn find_substring_last(line: &str, element: &str) ->Option<u32> {
 }
 
 /// Creates a lookup table to lookup string slices within each line
-/// 
+///
 /// Maps each string of a number to its digit
 fn load_table() -> LookupTable {
-    let table: LookupTable = LookupTable { 
+    let table: LookupTable = LookupTable {
         elements: vec![
             ("1".to_string(), 1),
             ("2".to_string(), 2),
@@ -101,13 +94,13 @@ fn load_table() -> LookupTable {
             ("7".to_string(), 7),
             ("8".to_string(), 8),
             ("9".to_string(), 9),
-             ],
-        };
+        ],
+    };
     table
 }
 
 /// Takes the table from part one and updates it with spelled versions of numbers
-/// 
+///
 /// Requires the original table as a reference
 fn update_table(table: &mut LookupTable) {
     let new_elements = vec![
@@ -131,6 +124,7 @@ struct Entry {
 }
 
 impl Entry {
+    /// Populate entry with default values
     fn new() -> Self {
         Entry {
             number: 0,
